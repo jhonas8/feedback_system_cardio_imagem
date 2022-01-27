b02420e61845e3bf54ac001a9a5d62f466d05571
Jhonas8 <love.android62@gmail.com>
2022-01-27 00:50:49 -0300

    Grid_and_font_alteration

--- client/src/components/Logo/index.tsx
+++ client/src/components/Logo/index.tsx
@@ -10,6 +10,7 @@ export default function Logo() {
   const render = (): JSX.Element => 
     <div className='Logo'>
       <div className='Cardio'>{LogoText.firstHalf}</div>
+      <div className='gap'/>
       <div className='Imagem'>{LogoText.secondHalf}</div>
     </div>
 
