3a770b4ec06e4369b70b65b1f51d88fb482c7326
Jhonas8 <love.android62@gmail.com>
2022-01-27 13:06:31 -0300

    ButtonFeedback_animated

--- client/src/components/FeedbackButton/index.tsx
+++ client/src/components/FeedbackButton/index.tsx
@@ -1,7 +1,11 @@
+import './Styles/css/styles.css'
+
 export default function FeedbackButton(this: any) {
   
   const render = () => (
-      <></>
+      <button className="FeedbackButton">
+          Feedback
+      </button>
   )
 
     return {
